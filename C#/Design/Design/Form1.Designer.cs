0c7849522965f737d6355db6724e7995a14f1b1f
秦晓宇 <qinxiaoyu@163.com>
2016-05-02 16:10:17 +0800

    将ehealth项目中的C#项目移植过来，并且改名为Design。

---
+++ C#/Design/Design/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace eHealth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gpsStatus = new System.Windows.Forms.Label();
+            this.Longitude = new System.Windows.Forms.Label();
+            this.latitude = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialBox
+            // 
+            this.serialBox.FormattingEnabled = true;
+            this.serialBox.Location = new System.Drawing.Point(83, 27);
+            this.serialBox.Name = "serialBox";
+            this.serialBox.Size = new System.Drawing.Size(75, 20);
+            this.serialBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SerialPort";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Location";
+            // 
+            // gpsStatus
+            // 
+            this.gpsStatus.AutoSize = true;
+            this.gpsStatus.Location = new System.Drawing.Point(13, 69);
+            this.gpsStatus.Name = "gpsStatus";
+            this.gpsStatus.Size = new System.Drawing.Size(0, 12);
+            this.gpsStatus.TabIndex = 6;
+            // 
+            // Longitude
+            // 
+            this.Longitude.AutoSize = true;
+            this.Longitude.Location = new System.Drawing.Point(13, 98);
+            this.Longitude.Name = "Longitude";
+            this.Longitude.Size = new System.Drawing.Size(59, 12);
+            this.Longitude.TabIndex = 7;
+            this.Longitude.Text = "Longitude";
+            // 
+            // latitude
+            // 
+            this.latitude.AutoSize = true;
+            this.latitude.Location = new System.Drawing.Point(13, 129);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(53, 12);
+            this.latitude.TabIndex = 8;
+            this.latitude.Text = "Latitude";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Star Numbers";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Not locate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(11, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(259, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(11, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 12);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Heart rate";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(81, 161);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 12);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 45);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 189);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 177);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 378);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.latitude);
+            this.Controls.Add(this.Longitude);
+            this.Controls.Add(this.gpsStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.serialBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox serialBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label gpsStatus;
+        private System.Windows.Forms.Label Longitude;
+        private System.Windows.Forms.Label latitude;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ C#/Design/Design/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.IO.Ports;
+
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace eHealth
+{
+    public partial class Form1 : Form
+    {
+        SerialPort com;
+        bool isConnect;
+       // delegate void HandleInterfaceUpdateDelegate(string text);  //委托，此为重点
+       // HandleInterfaceUpdateDelegate interfaceUpdateHandle;
+        Thread _readThread;
+        bool _keepReading;
+
+        String value = "0";
+        String longitude = "0";
+        String latitudeX = "0";
+
+        bool isLocated = false;
+
+        int mapflag = 0;
+
+        private System.Timers.Timer timer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            com = new SerialPort();
+            isConnect = false;
+            
+            //AT+ST=1
+            //AT+BT=123
+            //AT+LO=1234
+            //AT+LA=123
+
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+
+ 
+            Console.WriteLine("serial port");
+
+            String[] str = SerialPort.GetPortNames();
+            if (str == null)
+            {
+                MessageBox.Show("no port ", "error");
+                return;
+            }
+            foreach (String s in SerialPort.GetPortNames())
+            {
+                System.Console.WriteLine(s);
+                serialBox.Items.Add(s);
+            }
+            serialBox.SelectedIndex = 0;
+            button1.Text = "closed";
+           // interfaceUpdateHandle = new HandleInterfaceUpdateDelegate(UpdateTextBox);  //实例化委托对象
+            //this.com.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.OnDataRecivice);
+            com.ReceivedBytesThreshold = 1;
+
+    
+
+
+        }
+        private void OnDataRecivice(object sender ,SerialDataReceivedEventArgs e) {
+            byte[] readBuffer = new byte[com.ReadBufferSize];
+            com.Read(readBuffer, 0, readBuffer.Length);
+            //this.Invoke(interfaceUpdateHandle, new string[] { Encoding.Unicode.GetString(readBuffer) });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isConnect == false)
+            {
+                try
+                {
+                    com.BaudRate = 9600;
+                    Console.WriteLine(serialBox.Text);
+                    com.PortName = serialBox.Text;
+                    com.DataBits = 8;
+                    com.Open();
+                    isConnect = true;
+                    _keepReading = true;
+                    _readThread = new Thread(ReadPort);
+                    _readThread.Start();
+                    button1.Text = "opened";
+                }
+                catch (System.IO.IOException) {
+                    button1.Text = "closed";
+                    isConnect = false;
+                    MessageBox.Show("SerialPort Open FAIL", "ERROR");                
+                }
+                catch (System.NullReferenceException){
+                    button1.Text = "closed";
+                    isConnect = false;
+                    MessageBox.Show("Open SerialPort", "SerialPort FAIL");
+                }
+            }
+            else { 
+                
+            }
+        }
+
+        /*private void button2_Click(object sender, EventArgs e)
+        {
+            com.WriteLine("1234");
+            Console.WriteLine("1234");
+        }*/
+        private void UpdateTextBox(string text)
+        {
+            Console.WriteLine(text);
+            
+        }
+        private void ReadPort() {
+            while (_keepReading) {
+
+                if (isConnect == true){
+
+                    //byte[] readBuffer = new byte[com.ReadBufferSize + 1];
+                    try
+                    {
+
+                        String SerialIn = com.ReadLine();
+                        Console.WriteLine(SerialIn);
+
+                        String sssss = SerialIn;
+                        textBox1.Text = textBox1.Text + sssss + "\n\r";
+                        textBox1.SelectionStart = textBox1.Text.Length - 1;
+                        textBox1.ScrollToCaret();
+                        //sssss = "AT+BT=60\r\n";
+                        if (sssss.IndexOf("AT+ST=") == 0)
+                        {
+                            int i = sssss.IndexOf("AT+ST=") + 6;
+                            int j = sssss.IndexOf("\r\n");
+                            string strx = sssss.Substring(i, j - i + 2);
+                            Console.WriteLine("ST = " + strx);
+                            label8.Text = strx;
+                        }
+                        else if (sssss.IndexOf("AT+LO=") == 0)
+                        {
+                            int i = sssss.IndexOf("AT+LO=") + 6;
+                            int j = sssss.IndexOf('\n');
+                            string strx = sssss.Substring(i, sssss.Length - 6);
+                            Console.WriteLine("LO = " + strx);
+                            label6.Text = strx;
+                            longitude = strx;
+                            if (strx == "0")
+                            {
+                                label5.Text = "no locate";
+                                this.label5.ForeColor = Color.Red;
+                                isLocated = false;
+                            }
+                            else
+                            {
+                                label5.Text = "located";
+                                this.label5.ForeColor = Color.Green;
+                                isLocated = true;
+                            }
+                        }
+                        else if (sssss.IndexOf("AT+LA=") == 0)
+                        {
+                            int i = sssss.IndexOf("AT+LA=") + 6;
+                            int j = sssss.IndexOf('\n');
+                            string strx = sssss.Substring(i, sssss.Length - 6);
+                            Console.WriteLine("LA = " + strx);
+
+   
+                            label7.Text = "-"+strx;
+                            latitudeX = strx;
+
+
+           
+
+                           /* mapflag++;
+                            if (mapflag > 20)
+                            {
+                                mapflag = 0;
+                                HttpPost();
+                            }*/
+                            //HttpPost();
+                        }
+                        else if (sssss.IndexOf("AT+BT=") == 0)
+                        {
+                            int i = sssss.IndexOf("AT+BT=") + 6;
+                            int j = sssss.IndexOf('\n');
+                            Console.WriteLine("j = " + j);
+                            Console.WriteLine("i = " + i);
+                            Console.WriteLine(sssss.Length);
+                            string strx = sssss.Substring(i, sssss.Length - 6);
+                            Console.WriteLine("BT = " + strx);
+
+                            //   if (isLocated == true)
+                            {
+                                try
+                                {
+                                    int btInt = Int32.Parse(strx);
+                                     //if (btInt < 150)
+                                    {
+                                        label10.Text = strx;
+                                        value = strx;
+                                        Console.WriteLine(btInt);
+                                       // HttpPost();
+                                    }
+                                }
+                                catch (System.FormatException)
+                                {
+                                    Console.WriteLine("conver error");
+                                }
+
+                            }
+                        }
+
+
+                        //HttpPost(value, longitude, latitude);
+
+                    }
+                    catch (TimeoutException) { }
+                    catch (IOException) { }
+                }
+                else {
+                    TimeSpan waitTime = new TimeSpan(0,0,0,50);
+                    Thread.Sleep(waitTime);
+                }
+            } 
+        }
+        
+
+
+
+
+       /* private void button2_Click(object sender, EventArgs e)
+        {
+            //AT+ST=1
+            //AT+BT=123
+            //AT+LO=1234
+            //AT+LA=123
+            com.WriteLine("1234");
+            Console.WriteLine("1234");
+        }*/
+
+        private void HttpPost()
+        {
+          
+            try
+            {
+                int btInt = Int32.Parse(value);
+                if (btInt == 0) return;
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("conver error");
+                return;
+            }
+            
+
+
+            String server = "ge2-6061.cloud.thingworx.com";
+
+            String ss;
+            ss = "POST /Thingworx/Things/eHealth/Services/eHealthService?appKey=d2560f02-2622-4a8a-a3fc-e806a8a99f7c&method=post&x-thingworx-session=true&heartBeat=";
+            ss += value;//hrvData;
+            ss += "&";
+            ss += "longitude";
+            ss += "=";
+            ss += longitude;//gpsData.longitude;
+            ss += "&";
+            ss += "latitude";
+            ss += "=";
+            ss += "-";
+            ss += latitudeX;//gpsData.latitude;
+            ss += " HTTP/1.1\r\n";
+            ss += "Host: ge2-4022.cloud.thingworx.com\r\n";
+            ss += "Content-Type: text/html\r\n";
+            ss += "\r\n";
+
+
+
+            Console.WriteLine(ss);
+            try
+            {
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(server, 80);
+
+                NetworkStream ns = tcpClient.GetStream();
+
+
+                if (ns.CanWrite)
+                {
+                    Byte[] sendBytes = Encoding.UTF8.GetBytes(ss);
+                    ns.Write(sendBytes, 0, sendBytes.Length);
+                }
+                else
+                {
+                    MessageBox.Show("不能写入数据流", "终止", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    //Console.WriteLine("You cannot write data to this stream.");
+                    tcpClient.Close();
+
+                    // Closing the tcpClient instance does not close the network stream.
+                    ns.Close();
+                    return;
+                }
+                ns.Close();
+                tcpClient.Close();
+            }
+            catch (SocketException) { }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isConnect == true) {
+                _keepReading = false;
+                com.Close();
+                isConnect = false;
+            }
+            this.Close();
+            /*com.WriteLine("1234");
+            Console.WriteLine("1234");*/
+        }
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //模拟的做一些耗时的操作
+            Console.WriteLine("1234");
+            HttpPost();
+            System.Threading.Thread.Sleep(1000000);
+            System.Threading.Thread.Sleep(1000000);
+        }
+
+
+    }
+}
